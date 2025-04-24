5559d3d450ef670be75ad1e4e5a69b04c728fb5a
Harsh Sonkar <114162367+hq969@users.noreply.github.com>
2025-04-24 15:20:56 +0530

    Update app.py
    Signed-off-by: Harsh Sonkar <114162367+hq969@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,24 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
 import pickle
-import numpy as np
 
 app = Flask(__name__)
 
 # Load model and vectorizer
-model = pickle.load(open('ai_model/model.pkl', 'rb'))
-vectorizer = pickle.load(open('ai_model/vectorizer.pkl', 'rb'))
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+model = pickle.load(open("ai_model/model.pkl", "rb"))
+vectorizer = pickle.load(open("ai_model/vectorizer.pkl", "rb"))
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        expense_text = request.form['expense']
-        vect_text = vectorizer.transform([expense_text])
-        prediction = model.predict(vect_text)
-        return render_template('index.html', prediction=prediction[0])
+    data = request.get_json(force=True)
+    input_text = data['text']
+    vect = vectorizer.transform([input_text])
+    prediction = model.predict(vect)
+    return jsonify({'prediction': prediction[0]})
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
